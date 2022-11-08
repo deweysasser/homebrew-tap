3d2e3247c0916c16f70c90cbf8232184cf5ed4d7
Dewey Sasser <dewey@deweysasser.com>
2022-11-07 20:56:25 -0500

    Upgrade releasetool

--- releasetool.rb
+++ releasetool.rb
@@ -1,29 +1,29 @@
 class Releasetool < Formula
   desc ""
   homepage "https://github.com/deweysasser/releasetool"
-  version "v0.1"
+  version "v0.2"
 
   on_macos do
 
     if Hardware::CPU.intel?
-      url "https://github.com/deweysasser/releasetool/releases/download/v0.1/releasetool-darwin-amd64.zip"
-      sha256 "408981049779d2b4c8e12999320d28a8bfbb10973f97432fe3291216be81caa6"
+      url "https://github.com/deweysasser/releasetool/releases/download/v0.2/releasetool-darwin-amd64.zip"
+      sha256 "7002ed526076b85bc1a7f06adc1ecdc7b43dd1ad612a0ab7f829be2961d04603"
     end
 
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/deweysasser/releasetool/releases/download/v0.1/releasetool-darwin-arm64.zip"
-      sha256 "bd88557c875e87c45e54466a27bda519d18562023a3486671ef003ebf60c1cfd"
+      url "https://github.com/deweysasser/releasetool/releases/download/v0.2/releasetool-darwin-arm64.zip"
+      sha256 "482c5be5ef666ebffcbbbdcdbf905ca41e446341810c534872a51b0d1087ea82"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/deweysasser/releasetool/releases/download/v0.1/releasetool-linux-amd64.zip"
-      sha256 "ba2a6729b33a053abd6f56f323fe507c600a44bf42063aae365aeb50dfb9f06b"
+      url "https://github.com/deweysasser/releasetool/releases/download/v0.2/releasetool-linux-amd64.zip"
+      sha256 "f2f0d19a6442dc3c3056353a3c84a6954c65c8f34c3582231b3126931808e499"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/deweysasser/releasetool/releases/download/v0.1/releasetool-linux-arm64.zip"
-      sha256 "129c3b430b2b088359772f8131c3a879bed782fdc4c59a36957485f07e7041d8"
+      url "https://github.com/deweysasser/releasetool/releases/download/v0.2/releasetool-linux-arm64.zip"
+      sha256 "a88e4e4b3d173da51aa32cb159e2f1250f33abe81ddbc0b611271d34532326e5"
     end
   end
 
