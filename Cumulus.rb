4819aa355fa48a6b7e6655fbeb627d5894b6bdfd
Dewey Sasser <dewey@deweysasser.com>
2023-10-11 10:55:15 -0400

    feat: Update cumulus to 0.5.0

--- Cumulus.rb
+++ Cumulus.rb
@@ -1,29 +1,29 @@
 class Cumulus < Formula
   desc "Bulk access to multiple AWS clouds"
   homepage "https://github.com/deweysasser/cumulus"
-  version "v0.4.0"
+  version "v0.5.0"
 
   on_macos do
 
     if Hardware::CPU.intel?
-      url "https://github.com/deweysasser/cumulus/releases/download/v0.4.0/cumulus-darwin-amd64.zip"
-      sha256 "96c488e3bee935f224a874f855f35cff6900e177c527cbadacece06c78a34557"
+      url "https://github.com/deweysasser/cumulus/releases/download/v0.5.0/cumulus-darwin-amd64.zip"
+      sha256 "d833426e9c5ce8eb7a5acd9a49acb8e31f8ac14ca6f5f3273cadd70a510b5cb8"
     end
 
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/deweysasser/cumulus/releases/download/v0.4.0/cumulus-darwin-arm64.zip"
-      sha256 "0a713a81ffc85a1c30ac5c0d26548c233275d9e3070b79c90db60b3ac5761123"
+      url "https://github.com/deweysasser/cumulus/releases/download/v0.5.0/cumulus-darwin-arm64.zip"
+      sha256 "c1dea098f6c597392cbd3e90909a15b8e983a428145b2a1bdaf2550723242d7d"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/deweysasser/cumulus/releases/download/v0.4.0/cumulus-linux-amd64.zip"
-      sha256 "04192ffcaeaf6e6a56035c2e30652cdbfc252bfc0cd5dbd652c83b14ba48e914"
+      url "https://github.com/deweysasser/cumulus/releases/download/v0.5.0/cumulus-linux-amd64.zip"
+      sha256 "52ab5e466452c15d4cee4c9d7e8269a8021429b967a30261ad375438b572d8ae"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/deweysasser/cumulus/releases/download/v0.4.0/cumulus-linux-arm64.zip"
-      sha256 "89777045778e0de224210f9d5aa142710472d6be630cb28e9db03d97dcda2830"
+      url "https://github.com/deweysasser/cumulus/releases/download/v0.5.0/cumulus-linux-arm64.zip"
+      sha256 "3195ea54b19f18c1b16d5b35cf815168ac772dc848b5f1efbab506eb972342e0"
     end
   end
 
